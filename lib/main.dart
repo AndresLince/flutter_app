82cf74bac2f880594c78013d0ab78e6331c3e2b6
AndresLince <lynxdarmatron@gmail.com>
2023-06-09 06:43:54 -0500

    class 24

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+// The main function is the starting point for all ouf Flutter apps.
 void main() {
-  runApp(const MaterialApp(home: Text('Hello world')));
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text('Hello world'),
+      ),
+    ),
+  );
 }
