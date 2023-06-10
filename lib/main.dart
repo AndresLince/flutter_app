ac76ec6f14f3e1adef72467ba4d6b36105ab0a24
AndresLince <lynxdarmatron@gmail.com>
2023-06-10 15:59:47 -0500

    code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,16 @@ void main() {
   runApp(
     MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Im rich'),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      backgroundColor: Colors.blueGrey[900],
-          body: const Center(
-            child: Image(
-              image: NetworkImage('https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg?w=1272&h=954'),
+            appBar: AppBar(
+              title: const Text('Im rich'),
+              backgroundColor: Colors.blueGrey[900],
             ),
-          )
-    )),
+            backgroundColor: Colors.blueGrey[900],
+            body: const Center(
+              child: Image(
+                image: NetworkImage(
+                    'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg?w=1272&h=954'),
+              ),
+            ))),
   );
 }
