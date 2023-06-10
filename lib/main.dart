cca0283318ba22599ed9323803de15dfd097ef21
AndresLince <lynxdarmatron@gmail.com>
2023-06-10 12:00:21 -0500

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,15 @@ void main() {
     MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text('Im rich'),
+        title: const Text('Im rich'),
         backgroundColor: Colors.blueGrey[900],
       ),
       backgroundColor: Colors.blueGrey[900],
+          body: const Center(
+            child: Image(
+              image: NetworkImage('https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg?w=1272&h=954'),
+            ),
+          )
     )),
   );
 }
